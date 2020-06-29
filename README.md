0ca0115379503c6662c0046cf06aa35de24e8280
hhiromasa <hhiromasa217@gmail.com>
2020-06-29 19:58:29 +0900

    added comments and edited readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# 書籍管理アプリ
+
+## URL 説明
+
+- /
+- /bookshelf/ : 本棚。書籍管理のメインページ
+- /add_book/　: 本を追加するための form
+- /update_book/<int:book_id>/ : 本を更新するための form
+- /delete_book/<int:book_id>/ : 本を削除するための form
--- main.py
+++ main.py
@@ -7,11 +7,15 @@ app.secret_key = "123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite3'
 app.config['SQLALCHEMY_NATIVE_UNICODE'] = 'utf-8'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # disable warnings
-app.permanent_session_lifetime = timedelta(minutes=15)
+app.permanent_session_lifetime = timedelta(
+    minutes=15)  # セッション時間。使わないかも  6.29 hhiromasa
 
 db = SQLAlchemy(app)
 
+# -------------------------------------------models  6.29 hhiromasa-------------------------------------------
+
 """
+#ユーザ管理、本の感想文の投稿とかできたら面白そう。
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -42,7 +46,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     book_author = db.Column(db.String(100), nullable=False)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # lower case for table name
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # ユーザとの関連付け。ユーザ管理がまだ完成してないのでコメントアウト 6.29 hhiromasa
 
     def __repr__(self):
         return f"Book('{self.title}','{self.book_author}')"
@@ -52,10 +56,11 @@ class Book(db.Model):
 def home():  # ホームページ
     return render_template("index.html")
 
-# -------------------------------------------posts-------------------------------------------
+# -------------------------------------------posts  6.29 hhiromasa-------------------------------------------
 
 
 """
+#本の感想文の投稿とかできたら面白そう
 @ app.route("/post")
 def post():
     # DBに投稿追加
@@ -74,7 +79,7 @@ def delete_post():
     flash(f"投稿を削除しました", "info")
     return redirect(url_for("bookshelf"))
 """
-# -------------------------------------------book shelf-------------------------------------------
+# -------------------------------------------book shelf  6.29 hhiromasa-------------------------------------------
 
 
 @ app.route("/bookshelf/")
@@ -85,7 +90,7 @@ def bookshelf():  # 本棚(本一覧)を表示
 
 @ app.route("/add_book/", methods=["POST", "GET"])
 def add_book():
-    # DBに本追加
+    # もしrequestがPOSTだったら(formが送信されたら)formの内容をDBに追加
     if request.method == "POST":
         title = request.form["title"]
         book_author = request.form["book_author"]
@@ -94,23 +99,23 @@ def add_book():
         db.session.commit()
         flash(f"本を追加しました", "info")
         return redirect(url_for("bookshelf"))
-    else:
+    else:  # もしrequestがGETだったら(formを開いただけなら)formを表示
         return render_template("add_book.html")
 
 
 @ app.route("/update_book/<int:book_id>/", methods=["POST", "GET"])
 def update_book(book_id):
     book = Book.query.get_or_404(book_id)
-    # if post.author != current_user:
+    # if post.author != current_user: <-ユーザ管理だからあとで追加する
     #   abort(403)
-    if request.method == "POST":
+    if request.method == "POST":  # もしrequestがPOSTだったら(formが送信されたら)DBの内容を更新
         title = request.form["title"]
         book_author = request.form["book_author"]
         book.title = title
         book.book_author = book_author
         db.session.commit()
         flash(f"変更を保存しました", "info")
-    else:
+    else:  # 　もしrequestがGETだったら(formを開いただけなら)既存情報をformのvalueに入れた状態で表示
         title = book.title
         book_author = book.book_author
 
@@ -119,14 +124,16 @@ def update_book(book_id):
 
 @ app.route("/delete_book/<int:book_id>/", methods=["POST", "GET"])
 def delete_book(book_id):
+    # ここでDBから本を削除。削除し終わったらリダイレクト
+    # 多分「本当に削除しますか」って確認した方が親切
     book = Book.query.get_or_404(book_id)
     db.session.delete(book)
     db.session.commit()
     flash(f"本を削除しました", "info")
-    # ここでDBから本を削除。削除し終わったらリダイレクト
     return redirect(url_for("bookshelf"))
 
 
+# -------------------------------------------アプリをrun-------------------------------------------
 if __name__ == "__main__":
     db.create_all()  # create db
-    app.run(debug=True)  # debug = True is for debugging.
+    app.run(debug=True)  # debug = True はデバッグ用。
